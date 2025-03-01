5bdb96b3b3f30dcb07f1a493d74483d007d118de
Lenard <lenardrjc@gmail.com>
2025-02-28 19:03:17 -0600

    Put Lenard

--- index.js
+++ index.js
@@ -36,6 +36,67 @@ app.delete('/productos/:id', async (req, res) => {
   }
 });
 
+//Put
+app.put('/libros/:id', async (req, res) => {
+  const { id } = req.params;
+  const { librosTitulo, librosGenero, librosFechaPublicacion } = req.body;
+
+  try {
+    const result = await pool.query(
+      'UPDATE libros SET librosTitulo = $1, librosGenero = $2, librosFechaPublicacion = $3 WHERE librosID = $4 RETURNING *',
+      [librosTitulo, librosGenero, librosFechaPublicacion, id]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: 'Libro no encontrado' });
+    }
+
+    res.json({ message: 'Libro actualizado', updatedLibro: result.rows[0] });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put('/usuarios/:id', async (req, res) => {
+  const { id } = req.params;
+  const { usuariosNombre, usuariosCorreo, usuariosTelefono } = req.body;
+
+  try {
+    const result = await pool.query(
+      'UPDATE usuarios SET usuariosNombre = $1, usuariosCorreo = $2, usuariosTelefono = $3 WHERE usuariosID = $4 RETURNING *',
+      [usuariosNombre, usuariosCorreo, usuariosTelefono, id]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    res.json({ message: 'Usuario actualizado', updatedUsuario: result.rows[0] });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put('/prestamos/:id', async (req, res) => {
+  const { id } = req.params;
+  const { libros_ID, usuarios_ID, prestamosFechaInicial, prestamosFechaDevolucion } = req.body;
+
+  try {
+    const result = await pool.query(
+      'UPDATE prestamos SET libros_ID = $1, usuarios_ID = $2, prestamosFechaInicial = $3, prestamosFechaDevolucion = $4 WHERE prestamosID = $5 RETURNING *',
+      [libros_ID, usuarios_ID, prestamosFechaInicial, prestamosFechaDevolucion, id]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: 'Préstamo no encontrado' });
+    }
+
+    res.json({ message: 'Préstamo actualizado', updatedPrestamo: result.rows[0] });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
