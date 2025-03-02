975d49fc369e8e39959927ba9a62c13aaf60531d
LeeroyRivera <leeroya506@gmail.com>
2025-03-02 05:30:47 +0000

    añadido gitignore

--- db.js
+++ db.js
@@ -2,20 +2,21 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const PostgresDialect = 'postgres';
 
-const db = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     dialect: PostgresDialect,
     host: process.env.DB_HOST,
     port: 5432,
+    logging: false // Desactiva los logs de las consultas SQL
 });
 
 const connection = () => {
     db.authenticate()
     .then(() => {
-        console.log('Conexion a base de datos exitosa')
+        console.log('Conexion a base de datos exitosa');
     })
-    .catch( err => {
-        console.error('Error en conexion a base de datos', err.message)
-    })
+    .catch(err => {
+        console.error('Error en conexion a base de datos', err.message);
+    });
 }
 
 connection();
