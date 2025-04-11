4fb782b704b432c5726f9eafafda6213d1ff37e4
LeeroyRivera <leeroya506@gmail.com>
2025-04-11 10:30:33 +0000

    validaciones removidas

--- validaciones/prestamoValidaciones.js
+++ validaciones/prestamoValidaciones.js
@@ -4,8 +4,6 @@ const prestamoValidaciones = () => {
     return [
         body('libros_ID').isInt().withMessage('El ID del libro debe ser un número entero'),
         body('usuarios_ID').isInt().withMessage('El ID del usuario debe ser un número entero'),
-        body('prestamosFechaInicial').isDate().withMessage('Debe ser una fecha válida'),
-        body('prestamosFechaDevolucion').isDate().withMessage('Debe ser una fecha válida')
     ];
 };
 
